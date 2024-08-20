0e93554f3261a9d5dce286944e9e6acb5f32cb30
tejasprakash <tejas.prakash@celestialsys.com>
2024-08-20 13:15:02 +0530

    modified instance size

--- EKS/main.tf
+++ EKS/main.tf
@@ -31,22 +31,35 @@ module "vpc" {
 
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
   cluster_name    = "my-eks-cluster"
-  cluster_version = "1.24"
+  cluster_version = "1.30"
 
   cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_groups = {
-    nodes = {
+  # eks_managed_node_groups = {
+  #   nodes = {
+  #     min_size     = 1
+  #     max_size     = 3
+  #     desired_size = 2
+
+  #     instance_type = ["t2.small"]
+  #   }
+  # }
+
+    eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t2.small"]
+
       min_size     = 1
       max_size     = 3
       desired_size = 2
-
-      instance_type = ["t2.small"]
     }
   }
 
